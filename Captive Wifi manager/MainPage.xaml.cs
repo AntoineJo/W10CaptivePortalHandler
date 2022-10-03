fb3da9b634b2f954cc074d0bfdf5c1e2600ea395
AntoineJo <48328018+AntoineJo@users.noreply.github.com>
2022-10-03 23:25:56 +0200

    Add files via upload

--- Captive Wifi manager/MainPage.xaml.cs
+++ Captive Wifi manager/MainPage.xaml.cs
@@ -16,31 +16,51 @@ namespace Captive_Wifi_manager
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        enum Connectivity
+        {
+            NotDefined = 0,
+            NetworkError = -1,
+            OtherError = -2,
+            InternetConnected = 1,
+            CaptivePortalD = 2
+        }
+
+        
+        bool initialLaunch = true;
+        bool internetConnected = false;
+        Connectivity connectivityStatus = Connectivity.NotDefined;
+        string captivePortalUri = "";
+
         public MainPage()
         {
             this.InitializeComponent();
             NetworkStatusChange();
 
-            // Create a timer and set a two second interval.
+            // Create a timer and set a 10s interval to force a connectivity check in case the network connectivity status change event is not triggered
             var aTimer = new Timer();
             aTimer.Interval = 10000;
-
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += every10seconds;
-
-            // Have the timer fire repeated events (true is the default)
+            aTimer.Elapsed += everyXseconds;
             aTimer.AutoReset = true;
-
             // Start the timer
             aTimer.Enabled = true;
 
+            //Initial check
             onLaunch();
         }
 
-        private void every10seconds(object sender, ElapsedEventArgs e)
+        private void everyXseconds(object sender, ElapsedEventArgs e)
         {
             onLaunch();
         }
+
+        private void OnNewWindowRequested(WebView sender, WebViewNewWindowRequestedEventArgs e)
+        {
+
+            sender.Navigate(e.Uri);
+            e.Handled = true;
+
+        }
+
 
         void NetworkStatusChange()
         {
@@ -49,7 +69,7 @@ namespace Captive_Wifi_manager
             {
                 var networkStatusCallback = new NetworkStatusChangedEventHandler(OnNetworkStatusChange);
 
-                    NetworkInformation.NetworkStatusChanged += networkStatusCallback;
+                NetworkInformation.NetworkStatusChanged += networkStatusCallback;
             }
             catch (Exception ex)
             {
@@ -63,30 +83,29 @@ namespace Captive_Wifi_manager
 
             if (InternetConnectionProfile != null)
             {
-                // there is a connection available to something
-                var network = InternetConnectionProfile.GetNetworkConnectivityLevel();
-                switch (network)
-                {
-                    case NetworkConnectivityLevel.ConstrainedInternetAccess: notConnectedToInternet(); break;
-                    case NetworkConnectivityLevel.InternetAccess:
-                        if (await checkInternetConnectivity())
-                        {
-                            connectedToInternet();
-                        }
-                        else
-                        {
-                            notConnectedToInternet();
+                //Check the current connection status
+                bool currentConnection = await checkInternetConnectivity();
 
-                        }
+                //if the status is different that the one before or if this is the initial launch
+                if (internetConnected != currentConnection || initialLaunch)
+                {
+                    //Block or display the webview
+                    if (currentConnection)
+                    {
                         connectedToInternet();
-                        break;
-                    case NetworkConnectivityLevel.LocalAccess: break;
-                    case NetworkConnectivityLevel.None: break;
+                    }
+                    else
+                    {
+                        notConnectedToInternet();
+                    }
+                    initialLaunch = false;
+                    internetConnected = currentConnection;
                 }
+                //else do nothing
+
             }
             else
             {
-                //connectivityMaker.Text = "You are connected to: something or nothing, but not to Internet";
                 notConnectedToInternet();
             }
         }
@@ -96,18 +115,39 @@ namespace Captive_Wifi_manager
 
             try
             {
-                Uri testUri = new Uri("http://www.msftconnecttest.com/redirect");
+
+                //Using the Microsoft Edge captive portal detection mecanism (same as chromium detection https://source.chromium.org/chromium/chromium/src/+/main:components/captive_portal/core/captive_portal_detector.cc)
+                Uri testUri = new Uri("http://edge-http.microsoft.com/captiveportal/generate_204");
                 HttpClient client = new HttpClient();
                 HttpResponseMessage response = await client.GetAsync(testUri);
 
-                if (response.IsSuccessStatusCode)
+                // A 204 response code indicates there's no captive portal.
+                if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
                 {
+                    captivePortalUri = "";
                     return true;
                 }
                 else
                 {
+                    if(response.RequestMessage.RequestUri != testUri)
+                    {
+                        captivePortalUri = response.RequestMessage.RequestUri.AbsoluteUri;
+                    }
+                    else
+                    {
+                        captivePortalUri = "";
+                    }
                     return false;
                 }
+            }
+            catch (HttpRequestException httpException)
+            {
+                //Test if the certificate is not valid, in that case it is likely that we are behind a captive portal
+                if (httpException.HResult == -2147012858)
+                {
+                    return false;
+                }
+
             }
             catch (Exception e)
             {
@@ -138,7 +178,15 @@ namespace Captive_Wifi_manager
                 {
                     try
                     {
-                        webview.Navigate(new Uri("http://www.msftconnecttest.com/redirect"));
+                        if (captivePortalUri != "")
+                        {
+                            webview.Navigate(new Uri(captivePortalUri));
+                        }
+                        else
+                        {
+                            webview.Navigate(new Uri("http://www.msftconnecttest.com/redirect"));
+                        }
+
                         connectivityMaker.Text = "You are connected to: something or nothing, but not to Internet. Loading captive portal...";
                         webview.Visibility = Visibility.Visible;
                         blockText.Visibility = Visibility.Collapsed;
@@ -175,6 +223,8 @@ namespace Captive_Wifi_manager
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //force refresh by cleaning previous network connectivity status
+            initialLaunch = true;
             onLaunch();
         }
     }
